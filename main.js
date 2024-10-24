35409f161bc620670aaafc8a530968b6fc9aa4bc
Elias Michaias <emskeirik@gmail.com>
2024-10-24 10:51:52 -0400

    update export

--- main.js
+++ main.js
@@ -3,25 +3,6 @@ import { useState, useEffect } from "react";
 const $store = window.Alpine.store;
 const $effect = window.Alpine.effect;
 
-exports.useAlpineStore = (path) => {
-  const { parent, key } = walkPath(path);
-  const [value, setValue] = useState(parent[key]);
-
-  useEffect(() => {
-    $effect(() => {
-      setValue(parent[key]);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (parent[key] !== value) {
-      parent[key] = value;
-    }
-  }, [value]);
-
-  return [value, setValue];
-};
-
 const walkPath = (path) => {
   const parts = path.split(".");
   const firstPart = parts.shift();
@@ -36,4 +17,21 @@ const walkPath = (path) => {
   }
 
   return { parent: obj, key: parts[parts.length - 1] };
+};
+
+export const useAlpineStore = (path) => {
+  const { parent, key } = walkPath(path);
+  const [value, setValue] = useState(parent[key]);
+
+  useEffect(() => {
+    $effect(() => {
+      setValue(parent[key]);
+    });
+  }, []);
+
+  useEffect(() => {
+    parent[key] = value;
+  }, [value]);
+
+  return [value, setValue];
 };
